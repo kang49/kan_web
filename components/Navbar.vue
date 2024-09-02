d33d2d66313e59cfd80c7ac2a2a6c92ace6be951
Kankawee Aramrak <kankaweeisci123@gmail.com>
2024-09-02 22:55:24 +0700

    About me page mobile

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,29 +1,56 @@
 <template>
     <div class="w-full h-max fixed z-50">
-        <div class="flex px-[10px] justify-between transition-all duration-300"
-            :class="{ 'h-[40px] bg-transparent': !isToggleMenu, 'h-[200px] bg-black': isToggleMenu }">
-            <div class="pt-[10px]">
-                <h4 class="font-bold text-[16px]" :class="{ 'text-black': !isToggleMenu, 'text-white': isToggleMenu }">
-                    Kankawee Aramrak</h4>
+        <div class="px-[10px] transition-all duration-200" :class="{ 'h-[40px] bg-transparent': !isToggleMenu, 'h-[230px] bg-black': isToggleMenu }">
+            <div class="flex justify-between">
+                <div class="pt-[10px]">
+                    <h4 class="font-bold text-[16px]" :class="{ 'text-black': !isToggleMenu, 'text-white': isToggleMenu }">
+                        Kankawee Aramrak</h4>
+                </div>
+
+                <div class="max-h-[40px] pt-[4px] flex justify-center">
+                    <div>
+                        <!-- Hamburger mobile menu -->
+                        <button class="lg:hidden grid gap-1 p-2 rounded-md outline-none" @click="hamberBTN">
+                            <span
+                                v-bind:class="{ 'rotate-45 bg-white': isToggleMenu, 'translate-y-[9px]': isToggleMenu, 'bg-black': !isToggleMenu }"
+                                id="ham1" class="w-6 h-1 rounded-[10px] transition-all duration-200"></span>
+                            <span v-bind:class="{ 'opacity-0 bg-white': isToggleMenu, 'bg-black': !isToggleMenu }" id="ham2"
+                                class="w-6 h-1 rounded-[10px] transition-all duration-200"></span>
+                            <span
+                                v-bind:class="{ '-rotate-45 bg-white': isToggleMenu, '-translate-y-[7px]': isToggleMenu, 'bg-black': !isToggleMenu }"
+                                id="ham3" class="w-6 h-1 rounded-[10px] transition-all duration-200"></span>
+                        </button>
+                    </div>
+                </div>
             </div>
 
-            <div class="max-h-[40px] pt-[4px] flex justify-center">
-                <div>
-                    <!-- Hamburger mobile menu -->
-                    <button class="lg:hidden grid gap-1 p-2 rounded-md outline-none" @click="hamberBTN">
-                        <span
-                            v-bind:class="{ 'rotate-45 bg-white': isToggleMenu, 'translate-y-[9px]': isToggleMenu, 'bg-black': !isToggleMenu }"
-                            id="ham1" class="w-6 h-1 rounded-[10px] transition-all duration-200"></span>
-                        <span v-bind:class="{ 'opacity-0 bg-white': isToggleMenu, 'bg-black': !isToggleMenu }" id="ham2"
-                            class="w-6 h-1 rounded-[10px] transition-all duration-200"></span>
-                        <span
-                            v-bind:class="{ '-rotate-45 bg-white': isToggleMenu, '-translate-y-[7px]': isToggleMenu, 'bg-black': !isToggleMenu }"
-                            id="ham3" class="w-6 h-1 rounded-[10px] transition-all duration-200"></span>
-                    </button>
-
-                    <!--Menu Desktop-->
-                    <div class="space-x-2 text-white text-[16px] font-bold hidden lg:flex lg:items-center">
+            <!--Mobile Menu Detail -->
+            <div class="text-white text-center text-[16px] pt-[30px] flex justify-center" :class="{ 'hidden': !isToggleMenu }">
+                <div class="w-full h-full flex flex-col space-y-[5px]">
+                    <div class="flex justify-center space-x-[30px] items-center">
+                        <h4>EN</h4>
+                            <button @click="Page_Lang_Update" class="w-[60px] h-[20px] bg-white outline-none rounded-[10px] flex items-center relative">
+                                <div class="w-[15px] h-[15px] bg-black rounded-full absolute transition-all duration-200" :class="{ 'translate-x-[5px]': page_lang === 'EN', 'translate-x-[40px]': page_lang === 'TH' }"></div>
+                            </button>
+                        <h4>TH</h4>
                     </div>
+                    
+                    <div class="w-full flex justify-center py-[10px]">
+                        <div class="w-[40%] h-[3px] bg-white"></div>
+                    </div>
+
+                    <NuxtLink to="/">
+                        <h4 v-if="page_lang === 'EN'">Home</h4>
+                        <h4 v-if="page_lang === 'TH'">หน้าแรก</h4>
+                    </NuxtLink>
+                    <NuxtLink to="/">
+                        <h4 v-if="page_lang === 'EN'">Personal Blog</h4>
+                        <h4 v-if="page_lang === 'TH'">บล็อกส่วนตัว</h4>
+                    </NuxtLink>
+                    <NuxtLink to="/aboutme">
+                        <h4 v-if="page_lang === 'EN'">Contact</h4>
+                        <h4 v-if="page_lang === 'TH'">ติดต่อ</h4>
+                    </NuxtLink>
                 </div>
             </div>
         </div>
@@ -33,9 +60,25 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+//Var
 const isToggleMenu = ref(false);
+let page_lang = ref('EN');
+
+//Emit
+const emit = defineEmits(['update:page_lang']);
 
 function hamberBTN() {
     isToggleMenu.value = !isToggleMenu.value;
+}
+
+function Page_Lang_Update() {
+    if (page_lang.value === 'EN') {
+        page_lang.value = 'TH';
+        emit('update:page_lang', 'TH');
+    }
+    else {
+        page_lang.value = 'EN';
+        emit('update:page_lang', 'EN');
+    }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <template>
+    <Navbar @update:page_lang="Page_Lang_EmitHandler"/>
     <div class="w-full min-h-[calc(100dvh)]">
-        <Navbar />
-
         <!-- Page1 -->
         <div class="min-h-full w-full pt-[70px] flex">
             <div class="h-[70%] min-h-[600px] relative w-full flex items-center">
                 <div class="h-full w-[250px] absolute z-0 bg-[#E5C8AE]"></div>
                 <div class="w-[60%]">
-                    <NuxtImg src="/img/mypic1.png" class="z-10 relative h-[500px] ml-[30px] object-cover"></NuxtImg>
+                    <NuxtImg src="/img/mypic1.png" loading="lazy" class="z-10 relative h-[500px] ml-[30px] object-cover"></NuxtImg>
                 </div>
             </div>
             <div class="z-20 w-full absolute pt-[200px] flex justify-between">
@@ -25,7 +24,7 @@
                             ผู้ร่วมก่อตั้งทีมและวิศวกรซอฟท์แวร์ที่ TensorMiK</h4>
                     </div>
 
-                    <NuxtLink to="/">
+                    <NuxtLink to="/aboutme">
                         <div
                             class="border-black border-[2px] flex justify-center items-center w-max h-max mt-[20px] py-[10px] px-[15px]">
                             <h4 v-if="page_lang === 'EN'" class="font-normal text-[12px]">About me</h4>
@@ -39,18 +38,18 @@
         <!-- Page2 -->
         <div class="min-h-full w-full pt-[100px] px-[30px]">
             <div class="w-full h-[600px] flex justify-between items-center relative">
-                <NuxtImg src="/img/mypic2.png" class="h-full w-full object-cover"></NuxtImg>
+                <NuxtImg src="/img/mypic2.png" loading="lazy" class="h-full w-full object-cover"></NuxtImg>
                 <div class="w-full absolute flex justify-between">
                     <div class="w-[50%]"></div>
                     <div class="w-[50%] pr-[20px]">
                         <div>
-                            <h3 v-if="page_lang === 'EN'"class="text-white font-bold text-[16px] right-0 text-right">Software Development</h3>
-                            <h3 v-if="page_lang === 'TH'"class="text-white font-bold text-[16px] right-0 text-right">การพัฒนาซอฟท์แวร์</h3>
+                            <h3 v-if="page_lang === 'EN'" class="text-white font-bold text-[16px] right-0 text-right">Software Development</h3>
+                            <h3 v-if="page_lang === 'TH'" class="text-white font-bold text-[16px] right-0 text-right">การพัฒนาซอฟท์แวร์</h3>
                         </div>
 
                         <div class="pt-[10px]">
-                            <h4 v-if="page_lang === 'EN'"class="text-white font-normal text-[12px] right-0 text-right">"I believe that software development is a vital part of our present and future. This belief inspired me to start learning software development from a young age, with the hope of contributing to its advancement."</h4>
-                            <h4 v-if="page_lang === 'TH'"class="text-white font-normal text-[12px] right-0 text-right">"ผมเชื่อว่าการพัฒนาซอฟต์แวร์เป็นส่วนสำคัญสำหรับปัจจุบันและอนาคต ความเชื่อนี้เป็นแรงบันดาลใจให้ผมเริ่มเรียนรู้การพัฒนาซอฟต์แวร์ตั้งแต่วัยเด็ก ด้วยความหวังว่าจะได้มีส่วนร่วมในการขับเคลื่อนในด้านนี้"</h4>
+                            <h4 v-if="page_lang === 'EN'" class="text-white font-normal text-[12px] right-0 text-right">"I believe that software development is a vital part of our present and future. This belief inspired me to start learning software development from a young age, with the hope of contributing to its advancement."</h4>
+                            <h4 v-if="page_lang === 'TH'" class="text-white font-normal text-[12px] right-0 text-right">"ผมเชื่อว่าการพัฒนาซอฟต์แวร์เป็นส่วนสำคัญสำหรับปัจจุบันและอนาคต ความเชื่อนี้เป็นแรงบันดาลใจให้ผมเริ่มเรียนรู้การพัฒนาซอฟต์แวร์ตั้งแต่วัยเด็ก ด้วยความหวังว่าจะได้มีส่วนร่วมในการขับเคลื่อนในด้านนี้"</h4>
                         </div>
                     </div>
                 </div>
@@ -60,17 +59,17 @@
         <!-- Page3 -->
         <div class="min-h-full w-full pt-[100px] px-[30px]">
             <div class="w-full h-[600px] flex justify-between items-center relative">
-                <NuxtImg src="/img/starship.png" class="h-full w-full object-cover"></NuxtImg>
+                <NuxtImg src="/img/starship.png" loading="lazy" class="h-full w-full object-cover"></NuxtImg>
                 <div class="w-full absolute flex justify-between">
                     <div class="w-[50%] pl-[20px]">
                         <div>
-                            <h3 v-if="page_lang === 'EN'"class="text-white font-bold text-[16px] left-0">Space Technology</h3>
-                            <h3 v-if="page_lang === 'TH'"class="text-white font-bold text-[16px] left-0">เทคโนโลยีอวกาศ</h3>
+                            <h3 v-if="page_lang === 'EN'" class="text-white font-bold text-[16px] left-0">Space Technology</h3>
+                            <h3 v-if="page_lang === 'TH'" class="text-white font-bold text-[16px] left-0">เทคโนโลยีอวกาศ</h3>
                         </div>
 
                         <div class="pt-[10px]">
-                            <h4 v-if="page_lang === 'EN'"class="text-white font-normal text-[12px] left-0">"I'm fascinated by space technology because it embodies humanity's hope, breaking barriers and uniting global advancements into a single creation sent into space."</h4>
-                            <h4 v-if="page_lang === 'TH'"class="text-white font-normal text-[12px] left-0">"ผมหลงใหลในเทคโนโลยีอวกาศ เพราะมันคือความหวังของมนุษยชาติที่ช่วยให้เราทลายขีดจำกัด และรวมเอาความก้าวหน้าทางเทคโนโลยีทั่วโลกไว้ในสิ่งประดิษฐ์ที่ถูกส่งไปอวกาศ"</h4>
+                            <h4 v-if="page_lang === 'EN'" class="text-white font-normal text-[12px] left-0">"I'm fascinated by space technology because it embodies humanity's hope, breaking barriers and uniting global advancements into a single creation sent into space."</h4>
+                            <h4 v-if="page_lang === 'TH'" class="text-white font-normal text-[12px] left-0">"ผมหลงใหลในเทคโนโลยีอวกาศ เพราะมันคือความหวังของมนุษยชาติที่ช่วยให้เราทลายขีดจำกัด และรวมเอาความก้าวหน้าทางเทคโนโลยีทั่วโลกไว้ในสิ่งประดิษฐ์ที่ถูกส่งไปอวกาศ"</h4>
                         </div>
                     </div>
                     <div class="w-[50%]"></div>
@@ -84,17 +83,17 @@
                 <div class="w-full flex justify-between items-center">
                     <div class="w-[50%] pr-[20px]">
                         <div>
-                            <h3 v-if="page_lang === 'EN'"class="text-black font-bold text-[16px]">"Just do whatever you want to do."</h3>
-                            <h3 v-if="page_lang === 'TH'"class="text-black font-bold text-[16px]">แค่ทำอะไรก็ได้ที่อยากทำ</h3>
+                            <h3 v-if="page_lang === 'EN'" class="text-black font-bold text-[16px]">"Just do whatever you want to do."</h3>
+                            <h3 v-if="page_lang === 'TH'" class="text-black font-bold text-[16px]">แค่ทำอะไรก็ได้ที่อยากทำ</h3>
                         </div>
 
                         <div class="pt-[10px]">
-                            <h4 v-if="page_lang === 'EN'"class="text-black font-normal text-[12px]">"That's what my dad used to tell me when I was a kid. Those simple words have stayed with me, reminding me that freedom and passion are the keys to living a fulfilling life."</h4>
-                            <h4 v-if="page_lang === 'TH'"class="text-black font-normal text-[12px]">"นั่นคือคำที่พ่อเคยบอกผมไว้ตอนเด็กๆ คำพูดเรียบง่ายนี้ยังคงอยู่กับผมเสมอ คอยเตือนให้ผมรู้ว่าความอิสระและความหลงใหลคือกุญแจสำคัญของการมีชีวิตที่เติมเต็ม"</h4>
+                            <h4 v-if="page_lang === 'EN'" class="text-black font-normal text-[12px]">"That's what my dad used to tell me when I was a kid. Those simple words have stayed with me, reminding me that freedom and passion are the keys to living a fulfilling life."</h4>
+                            <h4 v-if="page_lang === 'TH'" class="text-black font-normal text-[12px]">"นั่นคือคำที่พ่อเคยบอกผมไว้ตอนเด็กๆ คำพูดเรียบง่ายนี้ยังคงอยู่กับผมเสมอ คอยเตือนให้ผมรู้ว่าความอิสระและความหลงใหลคือกุญแจสำคัญของการมีชีวิตที่เติมเต็ม"</h4>
                         </div>
                     </div>
                     <div class="w-[50%]">
-                        <NuxtImg src="/img/mypic3.png" class="h-full w-full object-cover"></NuxtImg>
+                        <NuxtImg src="/img/mypic3.png" loading="lazy" class="h-full w-full object-cover"></NuxtImg>
                     </div>
                 </div>
             </div>
@@ -103,17 +102,17 @@
         <!-- Page5 -->
         <div class="min-h-[720px] w-full pt-[100px]">
             <div class="w-full h-[450px] flex justify-center items-center relative">
-                <NuxtImg src="/img/highschoolfr.png" class="h-full w-full px-[30px] object-cover object-[-210px_0px] z-10"></NuxtImg>
+                <NuxtImg src="/img/highschoolfr.png" loading="lazy" class="h-full w-full px-[30px] object-cover object-[-210px_0px] z-10"></NuxtImg>
                 <div class="w-[70%] min-h-max pb-[20px] pt-[100px] bg-[#E3D583] absolute flex justify-end items-end bottom-[-170px] right-0 z-0">
-                    <div class="w-full bottom-[-200px] pr-[20px]">
+                    <div class="w-full bottom-[-200px] px-[20px]">
                         <div>
-                            <h3 v-if="page_lang === 'EN'"class="text-black font-bold text-[16px] text-right">My high school friends</h3>
-                            <h3 v-if="page_lang === 'TH'"class="text-black font-bold text-[16px] text-right">เพื่อนๆมัธยมปลาย</h3>
+                            <h3 v-if="page_lang === 'EN'" class="text-black font-bold text-[16px] text-right">My high school friends</h3>
+                            <h3 v-if="page_lang === 'TH'" class="text-black font-bold text-[16px] text-right">เพื่อนๆมัธยมปลาย</h3>
                         </div>
 
                         <div class="pt-[10px]">
-                            <h4 v-if="page_lang === 'EN'"class="text-black font-normal text-[12px] text-right">"They say the happiest and most fun times are during high school. Looking back, I couldn't agree more. Every moment was filled with laughter, adventures, and friendships."</h4>
-                            <h4 v-if="page_lang === 'TH'"class="text-black font-normal text-[12px] text-right">"เขาว่ากันว่าช่วงเวลาที่สนุกและมีความสุขที่สุดคือช่วงมัธยม มองย้อนกลับไปผมเห็นด้วยอย่างยิ่ง ทุกช่วงเวลาเต็มไปด้วยเสียงหัวเราะ การผจญภัย และมิตรภาพ"</h4>
+                            <h4 v-if="page_lang === 'EN'" class="text-black font-normal text-[12px] text-right">"They say the happiest and most fun times are during high school. Looking back, I couldn't agree more. Every moment was filled with laughter, adventures, and friendships."</h4>
+                            <h4 v-if="page_lang === 'TH'" class="text-black font-normal text-[12px] text-right">"เขาว่ากันว่าช่วงเวลาที่สนุกและมีความสุขที่สุดคือช่วงมัธยม มองย้อนกลับไปผมเห็นด้วยอย่างยิ่ง ทุกช่วงเวลาเต็มไปด้วยเสียงหัวเราะ การผจญภัย และมิตรภาพ"</h4>
                         </div>
                     </div>
                 </div>
@@ -127,8 +126,24 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 //Var
 let page_lang = ref('EN');
+function Page_Lang_EmitHandler(lang: string) {
+    if (lang === 'EN') {
+        page_lang.value = 'EN';
+    }
+    else {
+        page_lang.value = 'TH';
+    }
+}
+
+onMounted(() => {
+    //Animation Scripts
+    setTimeout(() => {
+        //Change Title
+        document.title = 'Kankawee Aramrak กันต์กวี อารามรักษ์ (Home)';
+    }, 200);
+});
 </script>
